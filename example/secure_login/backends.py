2656bede27bbff8ebce27f75633621830c714ab8
Shabda Raaj <shabda@agiliq.com>
2013-11-21 22:39:14 +0530

    WIP, trying to add another checker

--- example/secure_login/backends.py
+++ example/secure_login/backends.py
@@ -1,10 +1,15 @@
 from django.contrib.auth import backends
+from django.conf import settings
 
 import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class SecureLoginBackend(backends.ModelBackend):
 
+    def __init__(self, *args, **kwargs):
+        super(SecureLoginBackend, self).__init__(*args, **kwargs)
+        wordlist = os.path.join(BASE_DIR, "weakpasswords.txt")
+        self.weak_passwords = [el.strip() for el in open(wordlist).read().split()]
 
     def authenticate(self, username=None, password=None, **kwargs):
         if not self.no_weak_passwords(username, password, **kwargs):
@@ -12,9 +17,11 @@ class SecureLoginBackend(backends.ModelBackend):
         return super(SecureLoginBackend, self).authenticate(username, password, **kwargs)
 
     def no_weak_passwords(self, username=None, password=None, **kwargs):
-        wordlist = os.path.join(BASE_DIR, "weakpasswords.txt")
-        weak_passwords = [el.strip() for el in open(wordlist).read().split()]
-        if password in weak_passwords:
+
+        if password in self.weak_passwords:
             return False
         return True
+
+    def no_short_passwords(self, username=None, password=None, **kwargs):
+        if len(password) <
 
--- example/secure_login/tests.py
+++ example/secure_login/tests.py
@@ -4,7 +4,7 @@ from django.contrib.auth import authenticate
 
 class SecureLoginBackendTest(TestCase):
     def test_no_weak_passwords(self):
-        bad_password = "abc"
+        bad_password = "albatross"
         good_password = "a-l0ng-pa55w0rd-@^&"
         user = User.objects.create(username="hello")
         user.set_password("abc")
@@ -16,4 +16,13 @@ class SecureLoginBackendTest(TestCase):
         self.assertEqual(authenticate(username="hello", password=good_password), user)
 
 
+    def no_short_passwords(self):
+        bad_password = "123"
+        good_password = "a-l0ng-pa55w0rd-@^&"
+        user.set_password("abc")
+        user.save()
+        self.assertFalse(authenticate(username="hello", password=bad_password))
 
+        user.set_password(good_password)
+        user.save()
+        self.assertEqual(authenticate(username="hello", password=good_password), user)
