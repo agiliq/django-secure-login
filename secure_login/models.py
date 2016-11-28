d7de9b38e91655c9db63b2f81b475fa7abbea745
yashrastogi16 <yash.rastogi916@gmail.com>
2016-11-28 19:26:18 +0530

    updated models for pep8 convention

--- secure_login/models.py
+++ secure_login/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from django.conf import settings
 if settings.AUTH_USER_MODEL:
-	User = settings.AUTH_USER_MODEL
+    User = settings.AUTH_USER_MODEL
 else:
-	from django.contrib.auth.models import User
+    from django.contrib.auth.models import User
 
 
 class FailedLogin(models.Model):
